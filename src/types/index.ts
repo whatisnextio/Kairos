c4632a5fd5ba7e5576c41e0797f9177d249d4331
liammcdx <126476481+liammcdow@users.noreply.github.com>
2025-06-01 06:41:58 +0100

    Delete src/types/index.ts

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { DomainFocusData } from &#39;../store/useAppStore&#39;; // Assuming DomainFocusData is exported from useAppStore or defined elsewhere
-
-export interface IdentityAnchor {
-id: string;
-name: string;
-description: string;
-}
-export const CORE\_DOMAINS\_DEF: DomainFocusData['domain\_type'][] = ['BODY', 'LOVE', 'MISSION', 'SPIRIT'];
-export const PERSONAL\_OBJECTIVE\_DOMAIN\_DEF: DomainFocusData['domain\_type'] = 'PERSONAL\_OBJECTIVE';
